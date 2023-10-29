4e50d8048f0de7089d2a9de1f2d6f994ad9b4837
cyluss <cyluss@gmail.com>
2023-10-29 18:09:09 +0900

    add run on startup toggle

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ extern crate native_windows_derive as nwd;
 use chrono::{DateTime, Local};
 use nwd::NwgUi;
 use nwg::NativeUi;
-use std::env;
+use registry::{Hive, Security, Data};
 
 const UNKNOWN_ICON: &[u8] = include_bytes!("../assets/question-mark-4-16.ico");
 static VERSION: &'static str = include_str!("../assets/version.txt");
@@ -33,6 +33,10 @@ pub struct BasicApp {
     #[nwg_events(OnMenuItemSelected: [BasicApp::show_about])]
     tray_item_about: nwg::MenuItem,
 
+    #[nwg_control(parent: tray_menu, text: "Run on startup")]
+    #[nwg_events(OnMenuItemSelected: [BasicApp::toggle_run_on_startup])]
+    tray_item_run_on_startup: nwg::MenuItem,
+
     #[nwg_control(parent: tray_menu, text: "Exit")]
     #[nwg_events(OnMenuItemSelected: [BasicApp::exit])]
     tray_item_exit: nwg::MenuItem,
@@ -47,14 +51,48 @@ fn format_date(date: Option<DateTime<Local>>) -> String {
     }
 }
 
+fn get_run_on_startup() -> bool {
+    let regkey = Hive::CurrentUser.open(r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", Security::AllAccess);
+    let binding = std::env::current_exe().ok().expect("fff");
+    let current_exe_path = binding.to_str().expect("msg");
+
+    let run_on_startup_value = regkey.as_ref().expect("").value("KakaoAdblock");
+
+    return match run_on_startup_value {
+        Ok(value) => {
+            value.to_string() == current_exe_path.to_string()
+        }
+        Err(_) => {
+            false
+        }
+    };
+}
+
+fn toggle_run_on_startup() ->Result<(), registry::value::Error> {
+    let regkey = Hive::CurrentUser.open(r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", Security::AllAccess);
+    let binding = std::env::current_exe().ok().expect("fff");
+    let current_exe_path = binding.to_str().expect("msg");
+    let cep_ucs = utfx::U16CString::from_str(current_exe_path).unwrap();
+    
+    if get_run_on_startup() {
+        return regkey.as_ref().expect("no key").delete_value("KakaoAdblock");
+    } else {
+        return regkey.as_ref().expect("no key").set_value("KakaoAdblock", &Data::String(cep_ucs));
+    }
+}
+
 impl BasicApp {
     fn show_menu(&self) {
+        let run_on_startup_set = get_run_on_startup();
+        self.tray_item_run_on_startup.set_checked(run_on_startup_set);
+
         let (x, y) = nwg::GlobalCursor::position();
         self.tray_menu.popup(x, y);
     }
 
     fn show_about(&self) {
         let diag = shell_monitor::get_diagnostics();
+
         nwg::simple_message("About", &format!(
             "KakaoTalk Adblock for Win32 {}\n\
              \n\
@@ -69,6 +107,13 @@ impl BasicApp {
         ));
     }
 
+    fn toggle_run_on_startup(&self) {
+        let result = toggle_run_on_startup();
+        if result.is_err() {
+            nwg::fatal_message("Run On Startup - Error", &result.err().expect("").to_string());
+        }
+    }
+
     fn exit(&self) {
         nwg::stop_thread_dispatch();
     }
@@ -76,11 +121,7 @@ impl BasicApp {
 
 fn main() {
     unsafe { UP = Some(Local::now()) };
-
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 2 && args[1].eq("--debug") {
-        shell_monitor::set_config(shell_monitor::Config {debug: true});
-    }
+    // shell_monitor::set_config(shell_monitor::Config {debug: true});
 
     let hook = shell_monitor::run();
     shell_monitor::remove_ad_layout();
